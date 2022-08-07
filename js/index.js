620bfa215a2b799b9cc383c1e5b5b1dfa49ed7f3
ssakoda-wiccidu <ssakoda@wiccidu.jp>
2022-08-08 03:33:20 +0900

    add bootstrap

---
+++ js/index.js
@@ -0,0 +1,46 @@
+let port;
+
+async function onConnectButtonClick() {
+    try {
+        port = await navigator.serial.requestPort();
+        await port.open({ baudRate: 115200 });
+
+        while (port.readable) {
+            const reader = port.readable.getReader();
+
+            try {
+                while (true) {
+                    const { value, done } = await reader.read();
+                    if (done) {
+                        addSerial("Canceled\n");
+                        break;
+                    }
+                    const inputValue = new TextDecoder().decode(value);
+                    addSerial(inputValue);
+                }
+            } catch (error) {
+                addSerial("Error: Read" + error + "\n");
+            } finally {
+                reader.releaseLock();
+            }
+        }
+    } catch (error) {
+        addSerial("Error: Open" + error + "\n");
+    }
+}
+
+function addSerial(msg) {
+    var textarea = document.getElementById('outputArea');
+    textarea.value += msg;
+    textarea.scrollTop = textarea.scrollHeight;
+}
+
+async function sendSerial() {
+    var text = document.getElementById('sendInput').value;
+    document.getElementById('sendInput').value = "";
+
+    const encoder = new TextEncoder();
+    const writer = port.writable.getWriter();
+    await writer.write(encoder.encode(text + "\n"));
+    writer.releaseLock();
+}
